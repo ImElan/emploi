5c99b4b66a6d3106f864dfda9bd5101a418b0f5c
Elan <elansakthivel2000@gmail.com>
2021-01-23 14:08:36 +0530

    setup mongoDB cloud database

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
+
+const DATABASE = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+
+mongoose
+    .connect(DATABASE, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('Connected to database successfully...'));
 
 const PORT = process.env.port || 3000;
 
